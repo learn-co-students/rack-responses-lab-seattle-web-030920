4c078b466011414737b933028eb9fb31e4ea839f
djknudson <djknudson@icloud.com>
2020-03-30 13:09:02 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
-class Application(t)
+class Application
     def call(env)
         resp = Rack::Response.new
      
         morning = 'Good Morning!'
-        afternoon = 'Good Afternoon'
-        current_time = Time.new(t)
+        afternoon = 'Good Afternoon!'
+        current_time = Time.now
 
-        current_time.hour < 13 ? (resp.write "#{morning}") : (resp.write "#{afternoon}")
+        current_time.hour >= 12 ? (resp.write "#{afternoon}") : (resp.write "#{morning}")
      
         resp.finish
       end
